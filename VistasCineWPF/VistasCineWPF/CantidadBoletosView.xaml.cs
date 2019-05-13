1a4c27c022af69e8161a1c6a17781b9b2bd7ba34
PKfede <47314404+PKfede@users.noreply.github.com>
2019-05-13 16:03:36 -0500

    cine

---
+++ VistasCineWPF/VistasCineWPF/CantidadBoletosView.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VistasCineWPF
+{
+    /// <summary>
+    /// Lógica de interacción para CantidadBoletosView.xaml
+    /// </summary>
+    public partial class CantidadBoletosView : Window
+    {
+        public CantidadBoletosView()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCantBoletos_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnRegresarCant_Click(object sender, RoutedEventArgs e)
+        {
+            MenuPrincipal menu = new MenuPrincipal();
+            menu.Show();
+            this.Close();
+        }
+    }
+}
---
+++ VistasCineWPF/VistasCineWPF/MenuPrincipal.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VistasCineWPF
+{
+    /// <summary>
+    /// Lógica de interacción para MenuPrincipal.xaml
+    /// </summary>
+    public partial class MenuPrincipal : Window
+    {
+        public MenuPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCrearFunciones_Click(object sender, RoutedEventArgs e)
+        {
+            CrearFuncion crear = new CrearFuncion();
+            crear.Show();
+            this.Close();
+        }
+
+        private void BtnFunExisten_Click(object sender, RoutedEventArgs e)
+        {
+            FuncionExistente existente = new FuncionExistente();
+            existente.Show();
+            this.Close();
+        }
+
+        private void BtnCorte_Click(object sender, RoutedEventArgs e)
+        {
+            PuntodeCorte corte = new PuntodeCorte();
+            corte.Show();
+            this.Close();
+        }
+
+        private void BtnRegistro_Click(object sender, RoutedEventArgs e)
+        {
+            Historial historial = new Historial();
+            historial.Show();
+            this.Close();
+        }
+    }
+}
