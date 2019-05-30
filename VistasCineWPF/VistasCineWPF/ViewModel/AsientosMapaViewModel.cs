5c7709ad7f550c78386907f34bc3396c3b4557f4
PKfede <47314404+PKfede@users.noreply.github.com>
2019-05-30 12:28:31 -0500

    v.0algo

---
+++ VistasCineWPF/VistasCineWPF/ViewModel/AsientosMapaViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VistasCineWPF.Views;
+
+namespace VistasCineWPF.ViewModel
+{
+    class AsientosMapaViewModel:NotifyViewModel
+    {
+        private object view;
+        public AsientosMapaViewModel()
+        {
+            View = new UserControl2();
+        }
+
+        public object View
+        {
+            get { return view; }
+            set
+            {
+                view = value;
+                NotifyPropertyChanged("View");
+            }
+        }
+    }
+}
---
+++ VistasCineWPF/VistasCineWPF/Views/UserControl2.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VistasCineWPF.Views
+{
+    /// <summary>
+    /// Lógica de interacción para UserControl2.xaml
+    /// </summary>
+    public partial class UserControl2 : UserControl
+    {
+        public ObservableCollection<int> SeatsTop { get; private set; }
+        public ObservableCollection<int> SeatsMid { get; private set; }
+        public ObservableCollection<int> SeatsBot { get; private set; }
+
+        public UserControl2()
+        {
+            SeatsTop = new ObservableCollection<int>();
+            for (int i = 1; i <= 19; i++)
+            {
+           
+                    SeatsTop.Add(i);
+            }
+            SeatsMid = new ObservableCollection<int>();
+            for (int i = 1; i <= 102; i++)
+            {
+
+                SeatsMid.Add(i+19);
+            }
+            SeatsBot = new ObservableCollection<int>();
+            for (int i = 1; i <= 98; i++)
+            {
+
+                SeatsBot.Add(i+121);
+            }
+            InitializeComponent();
+        }
+    }
+}
