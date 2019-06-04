6203269445c35d00ca087c923f041665dbaf9cd1
luisIvanFuentesMariaca <luis.fuentes.mariaca@gmail.com>
2019-06-04 11:18:42 -0500

    Update Cine.sql

--- Cine.sql
+++ Cine.sql
@@ -3,15 +3,15 @@ CREATE DATABASE cine;
 USE cine;
 
 CREATE TABLE usuario(idTaquillero INT IDENTITY PRIMARY KEY NOT NULL, nombre VARCHAR(50), 
-apPaterno VARCHAR(50), apMaterno VARCHAR(50), telTaquillero VARCHAR (11), tipoUsuario VARCHAR(20),contraseña VARCHAR(20));
+apPaterno VARCHAR(50), apMaterno VARCHAR(50), telTaquillero VARCHAR (11), tipoUsuario VARCHAR(20),contra VARCHAR(20), nombre_usuario VARCHAR(10));
 
 
-CREATE TABLE pelicula(idPelicula INT PRIMARY KEY NOT NULL,nombre VARCHAR(50));
+CREATE TABLE pelicula(idPelicula INT PRIMARY KEY IDENTITY NOT NULL,nombre VARCHAR(50));
 
-CREATE TABLE funcion(idFuncion VARCHAR(50), fechaFuncion VARCHAR(50), horaFuncion VARCHAR(50),
+CREATE TABLE funcion(idFuncion INT IDENTITY NOT NULL, fechaFuncion VARCHAR(50), horaFuncion VARCHAR(50),
 asientosDisponibles INT, asientosOcupados INT, asientoAsignado VARCHAR(3), diagrama VARCHAR(220),
 precio INT, fk_idPelicula INT FOREIGN KEY REFERENCES pelicula(idPelicula), 
-CONSTRAINT PK_FUNCION PRIMARY KEY(idFuncion, precio));
+CONSTRAINT PK_FUNCION PRIMARY KEY(fk_idPelicula, fechaFuncion, horaFuncion));
 
 CREATE TABLE ventas(idVenta INT PRIMARY KEY IDENTITY, cantidad INT, hora VARCHAR(50), importe NUMERIC,
 fk_idTaquillero INT FOREIGN KEY REFERENCES usuario(idTaquillero), idFuncion VARCHAR(50), precio INT,
